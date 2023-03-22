d2caf88a253419a2e3557c6a9a77568dfa7a20fc
shihab17 <shihabahmed620@gmail.com>
2023-03-22 13:26:38 +0600

    swagger docs

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger.json')
 var indexRouter = require('./routes/index');
 require('dotenv').config();
 var app = express();
@@ -18,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-
+app.use('/api/v1/', indexRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
