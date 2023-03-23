2abc18aba8e03574d72d2a0ef4d39d3cf4a793a4
shihab17 <shihabahmed620@gmail.com>
2023-03-23 12:07:46 +0600

    file upload

--- services/fileService.js
+++ services/fileService.js
@@ -3,15 +3,26 @@ const upload = async (req, res) => {
     try {
         await uploadFile(req, res);
         if (req.file == undefined) {
-            return res.status(400).send({ message: "Upload a file please!" });
+            return { error: { message: "Upload a file please!" } }
         }
-        res.status(200).send({
-            message: "The following file was uploaded successfully: " + req?.file?.originalname || '',
-        });
+        return {
+            error: {
+                message: "The following file was uploaded successfully: " + req?.file?.originalname || '',
+            }
+        }
     } catch (err) {
-        res.status(500).send({
-            message: `Unable to upload the file: ${req?.file?.originalname}. ${err}`,
-        });
+        if (err.code == "LIMIT_FILE_SIZE") {
+            return {
+                error: {
+                    message: "File size cannot be larger than 2MB!",
+                }
+            }
+        }
+        return {
+            error: {
+                message: `Unable to upload the file: ${req?.file?.originalname}. ${err}`,
+            }
+        }
     }
 };
 
